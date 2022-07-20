6f4f728b57ed13f6dcab049a814fb95f64ff76c4
Daniel C <dalien@2a02-8440-3140-0501-20c5-c058-7b16-6ff0.rev.sfr.net>
2022-07-20 13:47:13 +0200

    deploy sh updated version

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e
 cd ~
+echo $(date '%H:%M:%S') >> ./log.txt
+[ -d "./temp_website" ] && sudo rm -r temp_website
+git clone https://github.com/DanielCadeau/goodvibesfrance.git temp_website
+declare last_deploy_date=`cat last_deploy_date.txt`
+cd temp_website
+declare last_commit_date=`git log -1 --format=%ct`
+cd ~
+if [[ $last_commit_date == $last_deploy_date ]]
+then
+	sudo rm -r temp_website
+	echo 'There is no new commit to deploy, abort.'
+	exit 1
+fi
 sudo rm -r website
-git clone https://github.com/DanielCadeau/goodvibesfrance.git website
+sudo mv temp_website website
 cp ./.env ./website/.env
 cd website
+echo $last_commit_date > ../last_deploy_date.txt
 npm i
 npm run build
 pm2 restart website
